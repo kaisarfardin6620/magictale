5ee4c328c48346bb2a7ff8c8b4a5c691e38da7bc
Abdullah Kaisar Fardin <kaisarfardin128@gmail.com>
2025-10-01 03:35:47 +0600

    adding more time to the jwt token

--- magictale/settings.py
+++ magictale/settings.py
@@ -132,8 +132,8 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),  
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'TOKEN_OBTAIN_SERIALIZER': 'authentication.serializers.MyTokenObtainPairSerializer',
